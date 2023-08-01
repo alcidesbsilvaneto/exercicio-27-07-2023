f0fcaeb4a388809191712a826facec515471f5eb
Alcides Bezerra <alcides.bsilvaneto@gmail.com>
2023-08-01 14:10:15 -0300

    Resposta

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Olá");
+import { gerarEmpresas } from "./gerar-empresas.js";
+
+const empresas = gerarEmpresas(100);
+
+let biggestOrder;
+let smallerOrder;
+
+for (const empresa of empresas) {
+  for (const pedido of empresa.pedidos) {
+    if (!biggestOrder || biggestOrder.valor < pedido.valor) {
+      biggestOrder = pedido;
+    }
+    if (!smallerOrder || smallerOrder.valor > pedido.valor) {
+      smallerOrder = pedido;
+    }
+  }
+}
+
+console.log(`Aqui está o maior pedido:`);
+console.log(biggestOrder);
+
+console.log("\nAqui está o menor pedido:");
+console.log(smallerOrder);
